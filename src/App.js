9d2e1f6e6286948c9019c447346d9bb4a687757e
Thomas <thomashaudiquer@gmail.com>
2022-07-19 10:19:13 +0100

    installing dependencies for the note app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from "./components/Sidebar"
+import Editor from "./components/Editor.js"
+import { data } from "./data"
+import Split from "react-split"
+import {nanoid} from "nanoid"
+import React, { useState } from "react";
 
-function App() {
+
+
+export default function App() {
+  /**
+   * Challenge:
+   * 1. Every time the `notes` array changes, save it 
+   *    in localStorage. You'll need to use JSON.stringify()
+   *    to turn the array into a string to save in localStorage.
+   * 2. When the app first loads, initialize the notes state
+   *    with the notes saved in localStorage. You'll need to
+   *    use JSON.parse() to turn the stringified array back
+   *    into a real JS array.
+   */
+  
+  const [notes, setNotes] = React.useState([])
+  const [currentNoteId, setCurrentNoteId] = React.useState(
+      (notes[0] && notes[0].id) || ""
+  )
+  
+  function createNewNote() {
+      const newNote = {
+          id: nanoid(),
+          body: "# Type your markdown note's title here"
+      }
+      setNotes(prevNotes => [newNote, ...prevNotes])
+      setCurrentNoteId(newNote.id)
+  }
+  
+  function updateNote(text) {
+      setNotes(oldNotes => oldNotes.map(oldNote => {
+          return oldNote.id === currentNoteId
+              ? { ...oldNote, body: text }
+              : oldNote
+      }))
+  }
+  
+  function findCurrentNote() {
+      return notes.find(note => {
+          return note.id === currentNoteId
+      }) || notes[0]
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <main>
+      {
+          notes.length > 0 
+          ?
+          <Split 
+              sizes={[30, 70]} 
+              direction="horizontal" 
+              className="split"
+          >
+              <Sidebar
+                  notes={notes}
+                  currentNote={findCurrentNote()}
+                  setCurrentNoteId={setCurrentNoteId}
+                  newNote={createNewNote}
+              />
+              {
+                  currentNoteId && 
+                  notes.length > 0 &&
+                  <Editor 
+                      currentNote={findCurrentNote()} 
+                      updateNote={updateNote} 
+                  />
+              }
+          </Split>
+          :
+          <div className="no-notes">
+              <h1>You have no notes</h1>
+              <button 
+                  className="first-note" 
+                  onClick={createNewNote}
+              >
+                  Create one now
+              </button>
+          </div>
+          
+      }
+      </main>
+  )
 }
-
-export default App;
